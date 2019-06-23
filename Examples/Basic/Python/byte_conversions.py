b851999f8470078b7ef400fda73d4a4d32d5fd80
JoRadiver <40248623+JoRadiver@users.noreply.github.com>
2019-06-23 15:01:43 +0200

    byte_conversions is now more useable

--- Examples/Basic/Python/byte_conversions.py
+++ Examples/Basic/Python/byte_conversions.py
@@ -4,64 +4,47 @@
 # ============== Bytes -> Normal ================ #
 # =============================================== #
 
+
+# gets strings stored in a bytestring.
+# special letters are replaced by "?"
+def string_from_byte(bytestring):
+	return bytestring.decode(encoding='utf-8', errors='ignore')
+
+
+# decodes numbers in a format like b'298.142'
+# this functions reads ints and floats like that.
+def number_from_bytes_literal(bytestring):
+	number = bytestring.decode(encoding='utf-8', errors='ignore')
+	return float(number)
+
+
 # gets a c++ uint8 from a byte(8bit unsigned integer)
 def uint8_from_byte(thebyte):
 	return int.from_byte(thebyte, byteorder="big")
 
 
-# gets the c++ char stored in a byte.
-# special letters are replaced by "-"
-def letter_from_byte(thebyte):
-	try
-
-
-# gets a float stored in 4 bytes.
-def float_from_4bytes(bytelist):
+# gets a c float stored in 4 bytes.
+def float_from_4bytes(bytestring):
 	pass
 
 
-# gets a float stored in 8 bytes.
-def float_from_8bytes(bytelist):
+# gets a c float stored in 8 bytes.
+def float_from_8bytes(bytestring):
 	pass
-
-
-# sometimes numbers get sent in a format 298.142
-# this functions reads ints and floats like that.
-def number_from_bytes_written(bytelist):
-	number = 0
-	decimal = 0
-	for b in bytelist:
-		letter = uint8_from_byte(b) - 0
-		if letter < 0:
-			break
-		else:
-			if b == b'.':
-				decimal = 1
-			elif decimal == 0:
-				number *= 10
-				number += letter
-			else:
-				number += letter/10
-				decimal += 1
-	return number
-
-
-# makes a normal string from a bytestring
-# special letters are replaced by "-"
-def string_from_bytestring(bytelist):
-	stringstore = ""
-	for b in bytelist:
-		stringstore.append(letter_from_byte(b))
-	return stringstore
-
 
 # =============================================== #
 # ============== Normal -> Bytes ================ #
 # =============================================== #
 
+
 # Encodes a string as bytes to be sent to the arduino.
-def bytelist_from_string(thestring):
-	pass
+def bytestring_from_string(thestring):
+	return thestring.encode(encoding='utf-8', errors='ignore')
+
+
+# makes numbers like b'123.321' from floats or ints
+def literal_number_from_number(thenumber):
+	return str(thenumber).encode(encoding='utf-8', errors='ignore')
 
 
 # Encodes a number as a c++ byte
@@ -73,7 +56,7 @@ def byte_from_uint8(number):
 
 
 # Encodes a larger number as an c++ int
-def bytes_from_unint16(number):
+def bytes_from_uint16(number):
 	pass
 
 
